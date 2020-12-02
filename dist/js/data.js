7d59853280007214222b54749250493e098db1ce
bikal1000 <bikal1000.bs@gmail.com>
2020-12-02 09:26:07 +0545

    basic style changes and dynamic content added

--- dist/js/data.js
+++ dist/js/data.js
@@ -24,9 +24,62 @@ const breakfast = [
         image: './assets/images/breakfast.jpg'
     },
 ]
+const lunch = [
+    {
+        name: 'lunch 1',
+        price: '250.00',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        serveTime: '2',
+        serveNos: '5',
+        image: './assets/images/lunch.jpeg'
+    },
+    {
+        name: 'lunch 2',
+        price: '350.00',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        serveTime: '6',
+        serveNos: '8',
+        image: './assets/images/lunch.jpeg'
+    },
+    {
+        name: 'lunch 3',
+        price: '450.00',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        serveTime: '9',
+        serveNos: '3',
+        image: './assets/images/lunch.jpeg'
+    },
+]
+const dinner = [
+    {
+        name: 'dinner 1',
+        price: '250.00',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        serveTime: '2',
+        serveNos: '5',
+        image: './assets/images/dinner.jpeg'
+    },
+    {
+        name: 'dinner 2',
+        price: '350.00',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        serveTime: '6',
+        serveNos: '8',
+        image: './assets/images/dinner.jpeg'
+    },
+    {
+        name: 'dinner 3',
+        price: '450.00',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        serveTime: '9',
+        serveNos: '3',
+        image: './assets/images/dinner.jpeg'
+    },
+]
 
 const breakfastDiv = $('#breakfast .content-row');
-
+const lunchDiv = $('#lunch .content-row');
+const dinnerDiv = $('#dinner .content-row');
 function dynamicFunc(name, price, description, serveTime, serveNos, image) {
     return ` 
     <div class="content-col">
@@ -58,4 +111,13 @@ function dynamicFunc(name, price, description, serveTime, serveNos, image) {
 breakfast.map(data => {
     const { name, image, price, description, serveNos, serveTime, } = data;
     breakfastDiv.append(dynamicFunc(name, price, description, serveTime, serveNos, image))
+})
+
+lunch.map(data => {
+    const { name, image, price, description, serveNos, serveTime, } = data;
+    lunchDiv.append(dynamicFunc(name, price, description, serveTime, serveNos, image))
+});
+dinner.map(data => {
+    const { name, image, price, description, serveNos, serveTime, } = data;
+    dinnerDiv.append(dynamicFunc(name, price, description, serveTime, serveNos, image))
 })
